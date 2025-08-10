dc0c26396737335496274c7d08a986f8149031aa
Nico Barberis <nico09barberis@gmail.com>
2025-08-10 15:34:57 -0300

    agregue nuevos enlaces en el footer

--- morse-code-translator/src/components/Footer.jsx
+++ morse-code-translator/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="w-full border-t border-black bg-[#090f1b] py-4 text-center text-sm text-[#F9FAFB]">
       © {new Date().getFullYear()} Barberis Nicolas. Todos los derechos reservados. 
       <a
-        href="https://github.com/tuusuario/morse-translator"
+        href="https://github.com/Nico09Barberis/morse-translator.git"
         target="_blank"
         rel="noopener noreferrer"
         className="hover:underline ml-4 hover:text-[#649cd3]"
@@ -14,12 +14,12 @@ const Footer = () => {
       </a>{" "}
       |{" "}
       <a
-        href="https://tupagina.com"
+        href="https://web-portafolio-phi.vercel.app/"
         target="_blank"
         rel="noopener noreferrer"
         className="hover:underline hover:text-[#649cd3]"
       >
-        Portfolio
+        Visita mi portafolio
       </a>
     </footer>
   );
